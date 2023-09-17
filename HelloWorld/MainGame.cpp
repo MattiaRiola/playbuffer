115b9fe005b752f58fbe9b5c681bda284f5016b0
MattiaRiola <s280169@studenti.polito.it>
2023-09-17 16:11:10 +0200

    feat: add background, player with movement and the fan

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -2,29 +2,109 @@
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
 
-int DISPLAY_WIDTH = 640;
-int DISPLAY_HEIGHT = 360;
-int DISPLAY_SCALE = 2;
+int DISPLAY_WIDTH = 1280;
+int DISPLAY_HEIGHT = 720;
+int DISPLAY_SCALE = 1;
 
-// The entry point for a PlayBuffer program
-void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
+
+struct GameState
 {
-	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+	int score = 0;
+};
+
+enum GameObjectType
+{
+	TYPE_NULL = -1,
+	TYPE_AGENT8,
+	TYPE_FAN,
+	TYPE_TOOL,
+	TYPE_COIN,
+	TYPE_STAR,
+	TYPE_LASER,
+	TYPE_DESTROYED,
+};
+
+void HandlePlayerControls();
+void UpdateFan();
+void UpdateTools();
+
+GameState gameState;
+// The entry point for a PlayBuffer program
+void MainGameEntry(PLAY_IGNORE_COMMAND_LINE)
+{
+	Play::CreateManager(DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE);
+	Play::CentreAllSpriteOrigins();
+	Play::LoadBackground("Data\\Backgrounds\\background.png");
+	Play::StartAudioLoop("music");
+	Play::CreateGameObject(TYPE_AGENT8, { 115, 0 }, 50, "agent8");
+	int id_fan = Play::CreateGameObject(TYPE_FAN, { 1140, 217 }, 0, "fan");
+	Play::GetGameObject(id_fan).velocity = { 0, 3 };
+	Play::GetGameObject(id_fan).animSpeed = 1.0f;
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
-bool MainGameUpdate( float elapsedTime )
+bool MainGameUpdate(float elapsedTime)
 {
-	Play::ClearDrawingBuffer( Play::cOrange );
-	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
+	Play::DrawBackground();
+	HandlePlayerControls();
+	UpdateFan();
+	UpdateTools();
 	Play::PresentDrawingBuffer();
-	return Play::KeyDown( VK_ESCAPE );
+	return Play::KeyDown(VK_ESCAPE);
 }
 
 // Gets called once when the player quits the game 
-int MainGameExit( void )
+int MainGameExit(void)
 {
 	Play::DestroyManager();
 	return PLAY_OK;
 }
 
+
+void HandlePlayerControls()
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8 ); 
+	if (Play::KeyDown(VK_UP))
+	{
+		obj_agent8.velocity = { 0,-4 };
+		Play::SetSprite(obj_agent8, "agent8_climb", 0.25f);
+	}
+	else if (Play::KeyDown(VK_DOWN))
+	{
+		obj_agent8.acceleration = { 0,1 };
+		Play::SetSprite(obj_agent8, "agent8_fall", 0);
+	}
+	else
+	{
+		Play::SetSprite(obj_agent8, "agent8_hang", 0.02f);
+		obj_agent8.velocity *= 0.5f;
+		obj_agent8.acceleration = { 0, 0 };
+	}
+	Play::UpdateGameObject(obj_agent8);
+	if (Play::IsLeavingDisplayArea(obj_agent8))
+		obj_agent8.pos = obj_agent8.oldPos;
+	Play::DrawLine({ obj_agent8.pos.x, 0 }, obj_agent8.pos, Play::cWhite);
+	Play::DrawObjectRotated(obj_agent8);
+
+}
+
+
+
+void UpdateFan()
+{
+	GameObject& obj_fan = Play::GetGameObjectByType(TYPE_FAN);
+
+	Play::DrawObject(obj_fan);
+}
+
+
+void UpdateTools()
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+	std::vector<int> vTools = Play::CollectGameObjectIDsByType(TYPE_TOOL);
+	for (int id : vTools)
+	{
+		GameObject& obj_tool = Play::GetGameObjectByType(id);
+	}
+
+}
